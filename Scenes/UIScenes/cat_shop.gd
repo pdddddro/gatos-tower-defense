1cdd7a77e64571f401ed45594d60dc1ba0f7b309
Pedro Lima <18pdro@gmail.com>
2025-05-19 15:28:21 -0300

    fix catshop menu

--- Scenes/UIScenes/cat_shop.gd
+++ Scenes/UIScenes/cat_shop.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var menu_size = 0.30
-var lerp_speed = 0.2
+var lerp_speed = .3
 
 var _popped_up = false
 var _up_anchor = Vector2(1-menu_size,1)
@@ -20,42 +20,36 @@ func _ready():
 	
 	cat_shop_container.visibility_layer = false
 	close_button.visibility_layer = false
-	self.mouse_filter = Control.MOUSE_FILTER_IGNORE 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	anchor_top = lerp(anchor_top,_target_anchor.x,lerp_speed)
 	anchor_bottom = lerp(anchor_bottom,_target_anchor.y,lerp_speed)
-	self.set_block_signals(not _popped_up)
+
+	if abs(anchor_top - _target_anchor.x) < 0.02 and abs(anchor_bottom - _target_anchor.y) < 0.02:
+		if _target_anchor == _down_anchor and cat_shop_container.visibility_layer:
+			cat_shop_container.visibility_layer = false
 
 func _on_close_pressed():
 	if !_popped_up:
-		
 		_target_anchor = _up_anchor
-		
 		cat_shop_container.visibility_layer = true
 		close_button.visibility_layer = true
 
 	else:
-		
-		self.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		_target_anchor = _down_anchor
-		cat_shop_container.visibility_layer = false
 		close_button.visibility_layer = false
+		_target_anchor = _down_anchor
 		
 	_popped_up = !_popped_up
 
 
 func _on_cat_shop_pressed() -> void:
 	if !_popped_up:
-		
 		cat_shop_container.visibility_layer = true
 		close_button.visibility_layer = true
-		
 		_target_anchor = _up_anchor
+		
 	else:
-		self.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		cat_shop_container.visibility_layer = false
 		close_button.visibility_layer = false
 		_target_anchor = _down_anchor
 	_popped_up = !_popped_up
