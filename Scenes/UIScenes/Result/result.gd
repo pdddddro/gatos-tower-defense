4434ee6a27a7cb0b9ba197cb586b1f9c63c6ef99
Pedro Lima <18pdro@gmail.com>
2025-06-21 11:25:27 -0300

    aaaaaaaaaa

--- Scenes/UIScenes/Result/result.gd
+++ Scenes/UIScenes/Result/result.gd
@@ -2,11 +2,16 @@ extends Control
 @onready var result_text_sprite = $VBoxContainer/Result
 var victory_result = false
 
+@onready var statistics_container = $VBoxContainer/Statistics
+@onready var best_cat = $VBoxContainer/BestCat
+
 func set_result(result: bool):
 	victory_result = result
 
 func _ready() -> void:
 	setup_result_ui()
+	statistics_container.visible = true
+	best_cat.visible = false
 
 func setup_result_ui():
 	if victory_result:
@@ -22,9 +27,23 @@ func _on_menu_pressed():
 	if scene_handler:
 		scene_handler.return_to_main_menu()
 
-func _on_new_game_pressed() -> void:
-	if scene_handler:
-		scene_handler.on_new_game_pressed()
+var screen_step = 0
+
+func _on_main_action_pressed() -> void:
+	if screen_step == 0:
+		$VBoxContainer/ActionButtons/MainAction/HBoxContainer/Label.text = "Novo Jogo"
+		$VBoxContainer/ActionButtons/MainAction/HBoxContainer/PlayIcon.visible = true
+		statistics_container.visible = false
+		best_cat.visible = true
+		
+		screen_step += 1
+		return
+		
+	if screen_step == 1:
+		if scene_handler:
+			scene_handler.on_new_game_pressed()
+			screen_step += 1  # (opcional, se quiser evitar múltiplos cliques)
+			return
 
 func update_stats_ui():
 	var enemies_defeated_label = $VBoxContainer/Statistics/Background/MarginContainer/HBoxContainer/Statistics/EnemiesDefeated/Number
@@ -34,14 +53,23 @@ func update_stats_ui():
 	var money_spent_label = $VBoxContainer/Statistics/Background/MarginContainer/HBoxContainer/Statistics/MoneySpent/Number
 	var number_of_cats = $VBoxContainer/Statistics/Background/MarginContainer/HBoxContainer/Statistics/NumberOfCats/Number
 	
-	enemies_defeated_label.text = str(GameData.enemies_defeated)
-	total_damage_label.text = str(GameData.total_damage)
-	fishs_collected_label.text = str(GameData.fishs_collected)
+	enemies_defeated_label.text = format_number(GameData.enemies_defeated)
+	total_damage_label.text = format_number(GameData.total_damage)
+	fishs_collected_label.text = format_number(GameData.fishs_collected)
 	time_in_game_label.text = format_time(GameData.time_in_game)
-	money_spent_label.text = str(GameData.money_spent)
-	number_of_cats.text = str(GameData.number_of_cats)
+	money_spent_label.text = format_number(GameData.money_spent)
+	number_of_cats.text = format_number(GameData.number_of_cats)
 
 func format_time(seconds: float) -> String:
 	var mins = int(seconds) / 60
 	var secs = int(seconds) % 60
 	return "%02d:%02d" % [mins, secs]
+
+func format_number(value: int) -> String:
+	if value < 1000:
+		return str(value)
+	elif value < 10000:
+		return "%.1fk" % (value / 1000.0)
+	else:
+		# Para valores >= 10000, remove a casa decimal
+		return "%dk" % int(round(value / 1000.0))
